fc6d75f76931e9096cb9c4a5f5f8581a2df25e7e
olneykimberly <olneykimberly@gmail.com>
2025-03-11 13:04:07 -0700

    snRNA final scripts - pass3

--- snRNAseq/scripts/14_find_markers_pass3.R
+++ snRNAseq/scripts/14_find_markers_pass3.R
@@ -2,11 +2,11 @@ knitr::opts_knit$set(root.dir = "/tgen_labs/jfryer/kolney/Ecoli_pigs/snRNAseq/sc
 setwd("/tgen_labs/jfryer/kolney/Ecoli_pigs/snRNAseq/scripts/")
 
 source(here::here("/tgen_labs/jfryer/kolney/Ecoli_pigs/bulk_RNAseq/scripts/", "file_paths_and_colours.R"))
-projectID <- "pigs_cellbender"
+projectID <- "pigs_cellbender_after_recluster_harm_int_noise_removed_after_annotation_FINAL"
 color.panel <- dittoColors()
 
 # read object
-dataObject <- readRDS(paste0("../rObjects/",projectID,"_after_recluster_harm_int.rds"))
+dataObject <- readRDS(paste0("../rObjects/",projectID,".rds"))
 # inspect
 dataObject
 DefaultAssay(dataObject) 
@@ -14,7 +14,7 @@ Layers(dataObject[["RNA"]])
 
 # set the default assay to RNA
 DefaultAssay(dataObject) <- "RNA"
-dataObject$seurat_clusters <- dataObject$SCT_snn_res.0.9
+dataObject$seurat_clusters <- dataObject$SCT_snn_res.0.8
 Idents(dataObject) <- "seurat_clusters"
 dataObject <- NormalizeData(dataObject)
 # inspect
@@ -29,16 +29,16 @@ markers <- SeuratWrappers::RunPrestoAll(
   only.pos = FALSE
 )
 write.table(markers, 
-            paste0("../results/markers/", projectID, "_after_recluster_harm_int_markers.tsv"),
+            paste0("../results/markers/", projectID, "_markers.tsv"),
             quote = FALSE,
             row.names = FALSE)
-saveRDS(markers, paste0("../rObjects/", projectID, "_after_recluster_harm_int_markers.rds"))
-markers <- readRDS(paste0("../rObjects/", projectID,"_after_recluster_harm_int_markers.rds"))
+saveRDS(markers, paste0("../rObjects/", projectID, "_markers.rds"))
+markers <- readRDS(paste0("../rObjects/", projectID,"_markers.rds"))
 
 # rearrange to order by cluster & filter to only include log2FC > 1 & FDR < 0.05
 all.markers.strict <- markers %>%
   group_by(cluster) %>%
   dplyr::filter(avg_log2FC > 1 & p_val_adj < 0.05)
 
-saveRDS(all.markers.strict, paste0("../rObjects/", projectID,"_after_recluster_harm_int_markers_log2FC1_q0.01.rds"))
-all.markers.strict <- readRDS(paste0("../rObjects/", projectID,"_after_recluster_harm_int_markers_log2FC1_q0.01.rds"))
+saveRDS(all.markers.strict, paste0("../rObjects/", projectID,"_markers_log2FC1_q0.01.rds"))
+all.markers.strict <- readRDS(paste0("../rObjects/", projectID,"_markers_log2FC1_q0.01.rds"))
---
+++ snRNAseq/scripts/file_paths_and_colours.R
@@ -0,0 +1,112 @@
+#----------------- Libraries
+library(remaCor)
+library(BiocParallel) 
+library(edgeR)  
+library(ggrepel) 
+library(ggplot2) 
+library(gplots) 
+library(grDevices)  
+library(stringr) 
+library(variancePartition) 
+library(tximport)
+library(tidyverse)
+library(GenomicFeatures)
+library(tximportData)
+#library(wasabi)
+#library(sleuth)
+library(dplyr)
+library(plyr)
+library(gridExtra)
+library(grid)
+library(lattice)
+library(data.table)
+library(glmGamPoi)
+
+
+#----------------- Define variables
+tissue <- c("Brain") # Kidney or Brain
+#control <- "Saline"
+#treatment <- "Ecoli"
+control_color <- "gray29"
+Ecoli_color <- "green"
+LPS_color <- "purple"
+output_dir <- c("") 
+typeOfCount <- c("STAR.bamReadsPerGene.out.tab") 
+pathToRef <- c("/research/labs/neurology/fryer/projects/references/pig/ensembl_v7/")
+
+#----------------- Data
+# read in Ecoli metadata
+Ecoli_meta <-
+  read.delim((
+    "/research/labs/neurology/fryer/projects/sepsis/pig/Ecoli/metadata.tsv"
+  ),
+  header = TRUE,
+  sep = "\t"
+  )
+Ecoli_meta <- Ecoli_meta[ -c(1)] # remove first column 
+Ecoli_meta$sample_name <- gsub("\\..*", "", Ecoli_meta$filename) # create sample_name column
+# create lane column 
+lane <- str_sub(Ecoli_meta$run_flowcell_lane,start=-1) 
+Ecoli_meta$lane <- paste0("L", lane)
+rm(lane)
+Ecoli_meta <- Ecoli_meta[Ecoli_meta$tissue == tissue, ]
+
+# path to counts files
+Ecoli_count_files <-
+  file.path(paste0(
+    "../starAligned/",
+    Ecoli_meta$sample_name,
+    "_", Ecoli_meta$lane, "_",
+    typeOfCount
+  ))
+# add sample name to counts files
+names(Ecoli_count_files) <- paste0(Ecoli_meta$sample)
+
+
+# sleuth and other tools requires path, sample and condition columns.
+# add this information to metadata
+Ecoli_meta$path <- Ecoli_count_files
+Ecoli_meta$sample <- Ecoli_meta$pig_id
+Ecoli_meta$condition <- as.factor(Ecoli_meta$group)
+
+
+# Read data LPS data
+# read in metadata
+LPS_meta <-
+  read.delim((
+    "/research/labs/neurology/fryer/projects/sepsis/pig/LPS/metadata.tsv"
+  ),
+  header = TRUE,
+  sep = "\t"
+  )
+# subset for tissue 
+LPS_meta <- LPS_meta[LPS_meta$tissue == tissue, ]
+
+# remove pigs 9 and 13
+LPS_meta <- LPS_meta[LPS_meta$pig_id != "9", ]
+LPS_meta <- LPS_meta[LPS_meta$pig_id != "13", ]
+LPS_meta <- LPS_meta[LPS_meta$blood_group != "BB", ]
+
+# path to counts files
+LPS_count_files <-
+  file.path(paste0(
+    "/research/labs/neurology/fryer/m239830/LPS_pigs/bulkRNA/starAligned/",
+    LPS_meta$featureCounts_name,"_",
+    typeOfCount
+  ))
+# add sample name to counts files
+names(LPS_count_files) <- paste0(LPS_meta$featureCounts_name)
+
+
+# sleuth and other tools requires path, sample and condition columns.
+# add this information to metadata
+LPS_meta$path <- LPS_count_files
+LPS_meta$sample <- LPS_meta$simplified_name
+LPS_meta$condition <- as.factor(LPS_meta$group)
+
+
+#----------------- Functions
+saveToPDF <- function(...) {
+  d = dev.copy(pdf,...)
+  dev.off(d)
+}
